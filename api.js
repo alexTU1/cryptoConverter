c340faa8b64e3078254a2bb49f91fa284e5c7a4a
alexTU1 <95954794+alexTU1@users.noreply.github.com>
2022-04-11 17:06:45 -0700

    Update api.js

--- api.js
+++ api.js
@@ -3,10 +3,11 @@ var app = express();
 var url = require('url');
 var pathName= ' ';
 
+
 pathName = '/';
 app.get(pathName, (req, res)=>{
     var message = 'This is the root webpage of this url';
-    res.send(message);
+    res.status(200).send(message);
     
 });
 
@@ -22,7 +23,7 @@ app.get(pathName, (req, res)=>{
     var usdConverter = (usdElement / 25000).toFixed(2); // converts USD to Bitcoin amount
 
     var testA = {amountInBitcoin: usdConverter + ' BIT'};       // stores the Bitcoin amount in JSON object
-    res.send(testA);                                   // displays the JSON object on the webpage
+    res.status(200).send(testA);                                   // displays the JSON object on the webpage
     
 });
 
@@ -38,18 +39,16 @@ app.get(pathName, (req, res)=>{
     var usdConverter = (usdElement / 900).toFixed(2); // converts USD to Etherium amount
     
     var testB = {amountInEtherium: usdConverter + ' ETH'};       // stores the Eitherium amount in JSON object
-    res.send(testB);                                        // displays the JSON object on the webpage
+    res.status(200).send(testB);                                        // displays the JSON object on the webpage
                                        
 });
 
-app.get(pathName, (req,res)=>{
-if(pathName != '/' || pathName != '/api/btc' || pathName != '/api/eth'){
+app.all('*', (req, res)=> {                        //sends 404 message to webpage
     console.log('Invalid Path Name');
-    res.writeHead(404);   
-}
+    res.status(404).send('404: Page not found.'); 
 });
 
 
-console.log('Navigate to http://localhost:3003/');
+console.log('Navigate to http://localhost:3003/'); //port number is interchangeable
 
-app.listen(3003);
+app.listen(3003); //port number is interchangeable
